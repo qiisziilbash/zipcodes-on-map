d218bbaf45fe3ce92524ac5387089d1f8b2d9f09
Mahdi Shafiei <m.shafiei0423@gmail.com>
2020-07-08 12:57:41 -0600

    init

---
+++ main.R
@@ -0,0 +1,9 @@
+# Zipcode data is downloaded  from http://download.geonames.org/export/zip/
+# Creative Commons Attribution 3.0 License
+# By Mahdi Sahfiei 2020-07-08
+
+
+# read the US zipcode data
+us_data <- read.delim('./Data/US/US.txt')
+
+zipcodes <- read.csv('./Data/zipcodes.csv')
